907b5f181950f41a84a714771a639d6319af7d88
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-05-26 16:40:02 +0200

    Try tempdir for mulled. Do not delete previously built packages.

--- bioconda_utils/build.py
+++ bioconda_utils/build.py
@@ -21,7 +21,6 @@ def purge():
     free = utils.get_free_space()
     if free < 10:
         logger.info("CLEANING UP PACKAGE CACHE (free space: %iMB).", free)
-        utils.run(["conda", "build", "purge-all"])
         utils.run(["conda", "clean" "--all"])
         logger.info("CLEANED UP PACKAGE CACHE (free space: %iMB).",
                     utils.get_free_space())
