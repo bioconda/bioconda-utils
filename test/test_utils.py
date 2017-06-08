55080bd624f0298d6b8a9c6b6fad8f87d08cfc72
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-06-08 11:58:32 +0200

    Debugging.

--- test/test_utils.py
+++ test/test_utils.py
@@ -242,6 +242,7 @@ def test_docker_build_image_fails():
 def test_conda_purge_cleans_up():
     def tmp_dir_exists(d):
         contents = os.listdir(d)
+        print(contents)
         for i in contents:
             if i.startswith('tmp') and '_' in i:
                 return True
