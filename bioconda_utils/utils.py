f7a413210e6420ee79c0e2033677d2906dfd415f
Marcel Bargull <marcel.bargull@udo.edu>
2018-07-04 17:37:58 +0200

    utils.get_dag: respect dependencies from build and run environments

--- bioconda_utils/utils.py
+++ bioconda_utils/utils.py
@@ -505,8 +505,14 @@ def get_dag(recipes, config, blacklist=None, restrict=True):
                        for meta, recipe in metadata)
     for meta, recipe in metadata:
         name = meta["package"]["name"]
-        dag.add_edges_from((dep, name)
-                           for dep in set(get_inner_deps(get_deps(meta, "host"))))
+        dag.add_edges_from(
+            (dep, name)
+            for dep in set(chain(
+                get_inner_deps(get_deps(meta, "build")),
+                get_inner_deps(get_deps(meta, "host")),
+                get_inner_deps(get_deps(meta, "run")),
+            ))
+        )
 
     return dag, name2recipe
 
