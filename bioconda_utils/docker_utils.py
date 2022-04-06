43c22aa5c970b3627c0815d50190d51e5aa161e0
Devon Ryan <dpryan79@gmail.com>
2022-04-06 22:36:04 +0200

    fix: Use mambabuild for generating the output file list

--- bioconda_utils/docker_utils.py
+++ bioconda_utils/docker_utils.py
@@ -105,7 +105,7 @@ conda config --add channels file://{self.container_staging} 2> >(
 conda mambabuild {self.conda_build_args} {self.container_recipe}/meta.yaml 2>&1
 
 # copy all built packages to the staging area
-cp `conda build {self.conda_build_args} {self.container_recipe}/meta.yaml --output` {self.container_staging}/{arch}
+cp `conda mambabuild {self.conda_build_args} {self.container_recipe}/meta.yaml --output` {self.container_staging}/{arch}
 conda index {self.container_staging}
 # Ensure permissions are correct on the host.
 HOST_USER={self.user_info[uid]}
