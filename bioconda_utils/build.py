11f812c174cb7158ec87ee56eb9ecf4eea5c4c0a
Johannes Köster <johannes.koester@uni-due.de>
2023-08-09 10:00:14 +0200

    do not instatiate RepoData, as the required methods are static

--- bioconda_utils/build.py
+++ bioconda_utils/build.py
@@ -118,10 +118,10 @@ def build(recipe: str, pkg_paths: List[str] = None,
                                         noarch=is_noarch)
             # Use presence of expected packages to check for success
             if (docker_builder.pkg_dir is not None):
-                platform = utils.RepoData().native_platform()
-                subfolder = utils.RepoData().platform2subdir(platform)
-                config = utils.load_conda_build_config(platform=subfolder)
-                pkg_paths = [p.replace(config.output_folder, docker_builder.pkg_dir) for p in pkg_paths]
+                platform = utils.RepoData.native_platform()
+                subfolder = utils.RepoData.platform2subdir(platform)
+                conda_build_config = utils.load_conda_build_config(platform=subfolder)
+                pkg_paths = [p.replace(conda_build_config.output_folder, docker_builder.pkg_dir) for p in pkg_paths]
             
             for pkg_path in pkg_paths:
                 if not os.path.exists(pkg_path):
