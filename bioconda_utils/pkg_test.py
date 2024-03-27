efa588a9efdba1fc07ffab3ee3866161c15859bd
Marcel Bargull <marcel.bargull@udo.edu>
2024-03-27 23:34:31 +0100

    feat: Use conda/conda-build instead of mamba/conda-mambabuild
    conda>=23.10 uses conda-libmamba-solver by default which means we don't
    need the solver injection via boa/conda-mambabuild anymore since it is
    handled upstream.
    
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- bioconda_utils/pkg_test.py
+++ bioconda_utils/pkg_test.py
@@ -162,8 +162,6 @@ def test_package(
     # galaxy-lib always downloads involucro, unless it's in cwd or its path is explicitly given.
     # We inject a POSTINSTALL to the involucro command with a small wrapper to
     # create activation / entrypoint scripts for the container.
-    # We also inject a PREINSTALL to alias conda to mamba so `mamba install` is
-    # used instead of `conda install` in the container builds.
     involucro_path = os.path.join(os.path.dirname(__file__), 'involucro')
     if not os.path.exists(involucro_path):
         raise RuntimeError('internal involucro wrapper missing')
