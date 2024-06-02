97f8c653d4c029dcff00f15d4632899d196dc802
Marcel Bargull <marcel.bargull@udo.edu>
2024-06-02 13:53:10 +0200

    feat: Update base-glibc-*-bash images to 3.1
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- bioconda_utils/build.py
+++ bioconda_utils/build.py
@@ -122,9 +122,9 @@ def build(recipe: str, pkg_paths: List[str] = None,
     is_noarch = bool(meta.get_value('build/noarch', default=False))
     use_base_image = meta.get_value('extra/container', {}).get('extended-base', False)
     if use_base_image:
-        base_image = 'quay.io/bioconda/base-glibc-debian-bash:3.0'
+        base_image = 'quay.io/bioconda/base-glibc-debian-bash:3.1'
     else:
-        base_image = 'quay.io/bioconda/base-glibc-busybox-bash:3.0'
+        base_image = 'quay.io/bioconda/base-glibc-busybox-bash:3.1'
 
     build_failure_record = BuildFailureRecord(recipe)
     build_failure_record_existed_before_build = build_failure_record.exists()
