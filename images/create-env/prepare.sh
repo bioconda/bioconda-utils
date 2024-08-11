cf71d12a20702ed6dc4dd4602db9d8d5821a8f8f
Ryan Dale <ryan.dale@nih.gov>
2024-08-11 15:19:19 +0000

    Revert "rm mamba from images"
    This reverts commit f311612d41f530b69fa9220f42e3d6db5c925840.

--- images/create-env/prepare.sh
+++ images/create-env/prepare.sh
@@ -5,7 +5,7 @@ BUILD_ARGS=()
 
 
 
-# Get the exact versions of conda that wasinstalled in build-env.
+# Get the exact versions of mamba and conda that were installed in build-env.
 #
 # If this tag exists on quay.io (that is, this create-env is being built in
 # a subsequent run), then use that. Otherwise, we assume this tag has already
@@ -21,10 +21,16 @@ CONDA_VERSION=$(
         podman run -t $REGISTRY/${BUILD_ENV_IMAGE_NAME}:${BIOCONDA_IMAGE_TAG} \
         bash -c "/opt/conda/bin/conda list --export '^conda$'| sed -n 's/=[^=]*$//p'"
 )
+MAMBA_VERSION=$(
+        podman run -t $REGISTRY/${BUILD_ENV_IMAGE_NAME}:${BIOCONDA_IMAGE_TAG} \
+        bash -c "/opt/conda/bin/conda list --export '^mamba$'| sed -n 's/=[^=]*$//p'"
+)
 # Remove trailing \r with parameter expansion
 export CONDA_VERSION=${CONDA_VERSION%$'\r'}
+export MAMBA_VERSION=${MAMBA_VERSION%$'\r'}
 
 BUILD_ARGS+=("--build-arg=CONDA_VERSION=$CONDA_VERSION")
+BUILD_ARGS+=("--build-arg=MAMBA_VERSION=$MAMBA_VERSION")
 
 # Needs busybox image to copy some items over
 if [ $(tag_exists $BASE_BUSYBOX_IMAGE_NAME $BASE_TAG) ]; then
