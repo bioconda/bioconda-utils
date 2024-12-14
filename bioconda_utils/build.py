0061429b2a8faa16f7a6d47cd31836c774ff8242
Ryan Dale <ryan.dale@nih.gov>
2024-12-13 19:58:56 -0500

    temporarily disable to make sure this line is problematic

--- bioconda_utils/build.py
+++ bioconda_utils/build.py
@@ -496,7 +496,8 @@ def build_recipes(recipe_folder: str, config_path: str, recipes: List[str],
             conda_build_purge()
             # prune stopped containers
             if docker_builder is not None:
-                docker_utils.pruneStoppedContainers()
+                pass
+                # docker_utils.pruneStoppedContainers()
 
     if failed or failed_uploads:
         logger.error('BUILD SUMMARY: of %s recipes, '
