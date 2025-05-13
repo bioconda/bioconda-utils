21a0c1ae24c30ff75f48a240410d4ca3222a3968
Ryan Dale <115406+daler@users.noreply.github.com>
2025-05-13 17:43:44 +0000

    clean .git dir before copying bioconda-utils over

--- images/bioconda-utils-build-env-cos7/prepare.sh
+++ images/bioconda-utils-build-env-cos7/prepare.sh
@@ -6,9 +6,15 @@ TAG="${BIOCONDA_UTILS_VERSION}_base$BASE_TAG"
 cp -r ../locale/C.utf8 .
 
 # Copy everything we need to install into this image. Note that we need the
-# .git directory so that versioneer can correctly compute the version.
+# .git directory so that versioneer can correctly compute the version within
+# the container.
 mkdir -p bioconda-utils
 for item in setup.py setup.cfg versioneer.py bioconda_utils MANIFEST.in .git; do
+  # Read-only files in a previously-copied .git dir will cause errors, so clean
+  # up first.
+  if [ -e "./bioconda-utils/.git" ]; then
+    rm -rf "./bioconda-utils/.git"
+  fi
   cp -ar ../../$item bioconda-utils
 done
 
@@ -17,5 +23,7 @@ done
 IS_BUILD_ENV=true
 
 BUILD_ARGS=()
+
+# Where to find the copied-over bioconda-utils
 BUILD_ARGS+=("--build-arg=BIOCONDA_UTILS_FOLDER=bioconda-utils")
 BUILD_ARGS+=("--build-arg=bioconda_utils_version=$BIOCONDA_UTILS_VERSION")
