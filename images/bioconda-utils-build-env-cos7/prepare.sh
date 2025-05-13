cab205f78d0cdff4621286af73f835d8a0bfa77d
Ryan Dale <115406+daler@users.noreply.github.com>
2025-05-13 18:57:20 +0000

    clone the entire repo to get it into the container

--- images/bioconda-utils-build-env-cos7/prepare.sh
+++ images/bioconda-utils-build-env-cos7/prepare.sh
@@ -1,3 +1,5 @@
+set -e
+
 source ../versions.sh
 IMAGE_NAME="${BUILD_ENV_IMAGE_NAME}"
 TAG="${BIOCONDA_UTILS_VERSION}_base$BASE_TAG"
@@ -5,18 +7,18 @@ TAG="${BIOCONDA_UTILS_VERSION}_base$BASE_TAG"
 # See ../locale/generate_locale.sh for how it was generated in the first place.
 cp -r ../locale/C.utf8 .
 
-# Copy everything we need to install into this image. Note that we need the
-# .git directory so that versioneer can correctly compute the version within
-# the container.
-mkdir -p bioconda-utils
-for item in setup.py setup.cfg versioneer.py bioconda_utils MANIFEST.in .git; do
-  # Read-only files in a previously-copied .git dir will cause errors, so clean
-  # up first.
-  if [ -e "./bioconda-utils/.git" ]; then
-    rm -rf "./bioconda-utils/.git"
-  fi
-  cp -ar ../../$item bioconda-utils
-done
+# We are aiming to get the entire repo into the container so that versioneer
+# can correctly determine the version. But the direc
+if [ -e "./bioconda-utils" ]; then
+  rm -rf "./bioconda-utils"
+fi
+git clone ../../ ./bioconda-utils
+current_branch=$(git symbolic-ref --short HEAD)
+(
+  cd bioconda-utils && git checkout $current_branch 
+)
+
+
 
 # The build script needs to special-case base images depending on archs when
 # building the build-env
