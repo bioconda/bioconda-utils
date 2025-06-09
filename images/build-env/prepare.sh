1534afc4023f66def24769678d4de752a649f4e1
Ryan Dale <115406+daler@users.noreply.github.com>
2025-06-09 12:59:22 +0000

    add build-env image

---
+++ images/build-env/prepare.sh
@@ -0,0 +1,27 @@
+set -e
+
+source ../image_config.sh
+IMAGE_NAME="${BUILD_ENV_IMAGE_NAME}"
+TAG="${BIOCONDA_UTILS_VERSION}_base$BASE_TAG"
+
+# See ../locale/generate_locale.sh for how it was generated in the first place.
+cp -r ../locale/C.utf8 .
+
+# We are aiming to get the entire repo into the container so that versioneer
+# can correctly determine the version.
+if [ -e "./bioconda-utils" ]; then
+  rm -rf "./bioconda-utils"
+fi
+git clone ../../ ./bioconda-utils
+
+# The build script needs to special-case base images depending on archs when
+# building the build-env
+IS_BUILD_ENV=true
+
+BUILD_ARGS=()
+
+# Where to find the copied-over bioconda-utils
+BUILD_ARGS+=("--build-arg=BIOCONDA_UTILS_FOLDER=bioconda-utils")
+BUILD_ARGS+=("--build-arg=bioconda_utils_version=$BIOCONDA_UTILS_VERSION")
+
+TEST_ARGS=()
