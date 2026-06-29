06b2ecf0675eefb8fc3d55f0a7ebc6e3d70e5f3c
benjamin <benjamin@stiffcoyote-e9263.tail6d1a6.ts.net>
2026-06-29 10:43:06 +0000

    fix: leaky tests
    test can leak build build artifacts, this makes those more robust

--- test/test_pkg_test.py
+++ test/test_pkg_test.py
@@ -43,49 +43,61 @@ one:
 """)
 
 
-# Skip mulled_test on default since we already run pkg_test.test_package for every test case.
-def _build_pkg(recipe, mulled_test=False, docker_builder=None):
-    r = Recipes(recipe, from_string=True)
-    r.write_recipes()
-    recipe = r.recipe_dirs["one"]
-    built_packages = utils.built_package_paths(recipe)
-    for pkg in built_packages:
-        ensure_missing(pkg)
-    build.build(
-        recipe=r.recipe_dirs["one"],
-        pkg_paths=built_packages,
-        mulled_test=mulled_test,
-        docker_builder=docker_builder,
-    )
-    return built_packages
+@pytest.fixture
+def build_pkg(request):
+    """Build test packages and remove every registered output afterward."""
+    registered_packages = set()
+
+    def _build_pkg(recipe, mulled_test=False, docker_builder=None):
+        r = Recipes(recipe, from_string=True)
+        r.write_recipes()
+        recipe_dir = r.recipe_dirs["one"]
+        built_packages = utils.built_package_paths(recipe_dir)
+        registered_packages.update(built_packages)
+        for pkg in built_packages:
+            ensure_missing(pkg)
+        build.build(
+            recipe=recipe_dir,
+            pkg_paths=built_packages,
+            mulled_test=mulled_test,
+            docker_builder=docker_builder,
+        )
+        return built_packages
+
+    def remove_built_packages():
+        for pkg in registered_packages:
+            ensure_missing(pkg)
+
+    request.addfinalizer(remove_built_packages)
+    return _build_pkg
 
 
 @pytest.mark.skipif(SKIP_OSX, reason="skipping on osx")
-def test_pkg_test():
+def test_pkg_test(build_pkg):
     """
     Running a mulled-build test shouldn't cause any errors.
     """
-    built_packages = _build_pkg(RECIPE_ONE)
+    built_packages = build_pkg(RECIPE_ONE)
     for pkg in built_packages:
         pkg_test.test_package(pkg)
 
 
 @pytest.mark.skipif(SKIP_OSX, reason="skipping on osx")
-def test_pkg_test_mulled_build_error():
+def test_pkg_test_mulled_build_error(build_pkg):
     """
     Make sure calling mulled-build with the wrong arg fails correctly.
     """
-    built_packages = _build_pkg(RECIPE_ONE)
+    built_packages = build_pkg(RECIPE_ONE)
     with pytest.raises(sp.CalledProcessError):
         for pkg in built_packages:
             pkg_test.test_package(pkg, mulled_args="--wrong-arg")
 
 
 @pytest.mark.skipif(SKIP_OSX, reason="skipping on osx")
-def test_pkg_test_custom_base_image():
+def test_pkg_test_custom_base_image(build_pkg):
     """
     Running a mulled-build test with a custom base image.
     """
-    built_packages = _build_pkg(RECIPE_CUSTOM_BASE)
+    built_packages = build_pkg(RECIPE_CUSTOM_BASE)
     for pkg in built_packages:
         pkg_test.test_package(pkg, base_image="debian:latest")
